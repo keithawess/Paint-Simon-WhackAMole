1a6a800fd1eed24b5e98e545e5e62fdfe09f822d
keithawess <61606760+keithwess@users.noreply.github.com>
2021-06-13 18:26:50 -0500

    Adds Ability to add up to 8 colors.

--- Paint/paint.js
+++ Paint/paint.js
@@ -7,8 +7,10 @@ let colorBox = document.getElementById('colorBox');
 let red = redSlider.value;
 let blue = blueSlider.value;
 let green = greenSlider.value;
+let selectedColor = "rgb(255,0,0)";
+let numColors = 3;
 
-let addedColors = ["rgb(255,0,0)", "rgb(0,255,0", "rgb(255,255,0)"]
+addColorEvents();
 
 redSlider.addEventListener("mousedown", (e) => {
     redSlider.addEventListener("mousemove", (e) => {
@@ -31,3 +33,32 @@ blueSlider.addEventListener("mousedown", (e) => {
     })
 })
 
+addColorButton.addEventListener("click", (e) => {
+    addColor();
+})
+
+function addColorEvents(){
+    let colors = document.getElementsByClassName('addedColor');
+    for (i = 0; i < colors.length; i++)
+    {
+        colors[i].addEventListener("click", (e) => {
+            document.querySelector(".selected").classList.remove('selected');
+            e.target.classList.add('selected');
+            selectedColor = e.target.value;
+            console.log(selectedColor);
+        })
+    }
+}
+
+function addColor(){
+    if(numColors < 8)
+    {
+        let added = document.createElement('button');
+        added.classList.add('addedColor');
+        added.value = `rgb(${red},${green},${blue})`;
+        added.style.backgroundColor = `rgb(${red},${green},${blue})`;
+        colorBox.append(added);
+        addColorEvents();
+        numColors++;
+    }
+}
