2c10a7050abe8f606a473e9da94769926d6bc625
keithawess <61606760+keithwess@users.noreply.github.com>
2021-06-13 23:36:11 -0500

    Addes button input, checking logic, needs testing

--- Simon/simon.js
+++ Simon/simon.js
@@ -3,55 +3,106 @@ const redButton = document.getElementById('redButton');
 const blueButton = document.getElementById('blueButton');
 const yellowButton = document.getElementById('yellowButton');
 const score = document.getElementById('score');
+const green = 0;
+const red = 1;
+const blue = 2;
+const yellow = 3;
 let pattern = [];
+let userInputList = [];
+let userTurn = false;
+
+greenButton.addEventListener ("click", (e) => {
+    if (userTurn === true)
+    {
+        userInputList.push(green);
+    }
+})
+
+redButton.addEventListener ("click", (e) => {
+    if (userTurn === true)
+    {
+        userInputList.push(red);
+    }
+})
+
+blueButton.addEventListener ("click", (e) => {
+    if (userTurn === true)
+    {
+        userInputList.push(blue);
+    }
+})
+
+yellowButton.addEventListener ("click", (e) => {
+    if (userTurn === true)
+    {
+        userInputList.push(yellow);
+    }
+})
 
 function addStep() {
     pattern.push(Math.floor(Math.random() * 3));
 }
 
+function userInput() {
+    userTurn = true;
+    let temp = setTimeout(() => {
+        userTurn = false;
+        if (checkTurn())
+        {
+            addStep();
+            playPattern(0);
+        } else {
+            gameOver();
+        }
+    }, pattern.length * 2000)
 
-
-addStep();
-addStep();
-addStep();
-addStep();
-addStep();
-
-playPattern(0);
+}
 
 function playPattern(i) {
-    if (i < pattern.length)
-    switch (pattern[i]) {
-        case 0:
-            greenButton.style.backgroundColor = "rgba(0,190,16,1)";
-            setTimeout(() => {
-                greenButton.style.backgroundColor = "rgba(0,190,16,.5)";
-                playPattern(++i);
-            }, 1000)
-            break;
-
-        case 1:
-            redButton.style.backgroundColor = "rgba(255, 0, 0, 1)";
-            setTimeout(() => {
-                redButton.style.backgroundColor = "rgba(255, 0, 0, .5)";
-                playPattern(++i);
-            }, 1000)
-            break;
-
-        case 2:
-            blueButton.style.backgroundColor = "rgba(0, 0, 255, 1)";
-            setTimeout(() => {
-                blueButton.style.backgroundColor = "rgba(0, 0, 255, .5)";
-                playPattern(++i);
-            }, 1000)
-            break;
-
-        case 3:
-            yellowButton.style.backgroundColor = "rgba(255, 255, 0, 1)";
-            setTimeout(() => {
-                yellowButton.style.backgroundColor = "rgba(255, 255, 0, .5)";
-                playPattern(++i);
-            }, 1000)
-            break;
+    if (i < pattern.length){ 
+        switch (pattern[i]) {
+            case 0:
+                greenButton.style.backgroundColor = "rgba(0,190,16,1)";
+                setTimeout(() => {
+                    greenButton.style.backgroundColor = "rgba(0,190,16,.5)";
+                    playPattern(++i);
+                }, 1000)
+                break;
+    
+            case 1:
+                redButton.style.backgroundColor = "rgba(255, 0, 0, 1)";
+                setTimeout(() => {
+                    redButton.style.backgroundColor = "rgba(255, 0, 0, .5)";
+                    playPattern(++i);
+                }, 1000)
+                break;
+    
+            case 2:
+                blueButton.style.backgroundColor = "rgba(0, 0, 255, 1)";
+                setTimeout(() => {
+                    blueButton.style.backgroundColor = "rgba(0, 0, 255, .5)";
+                    playPattern(++i);
+                }, 1000)
+                break;
+    
+            case 3:
+                yellowButton.style.backgroundColor = "rgba(255, 255, 0, 1)";
+                setTimeout(() => {
+                    yellowButton.style.backgroundColor = "rgba(255, 255, 0, .5)";
+                    playPattern(++i);
+                }, 1000)
+                break;
+        }
+    } else {
+        userInput();
     }
+}
+
+function checkTurn() {
+    let success = true;
+    for (let i = 0; i < pattern.length; i++) {
+        if (pattern[i] !== userInputList[i])
+        success = false;   
+    }
+    return success;
 }
