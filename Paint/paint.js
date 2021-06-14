3e3e85e2d6b06b944e8441a7c1ac560b6bc33201
keithawess <61606760+keithwess@users.noreply.github.com>
2021-06-13 19:32:41 -0500

    Adds ability to clear canvas and palette

--- Paint/paint.js
+++ Paint/paint.js
@@ -1,11 +1,13 @@
-const colorViewer = document.getElementById('colorViewer');
-const redSlider = document.getElementById('redSlider');
-const blueSlider = document.getElementById('blueSlider');
-const greenSlider = document.getElementById('greenSlider');
-const addColorButton = document.getElementById('colorButton');
-const colorBox = document.getElementById('colorBox');
-const drawingPad = document.getElementById('drawingPad');
-const ctx = drawingPad.getContext('2d');
+const colorViewer = document.getElementById("colorViewer");
+const redSlider = document.getElementById("redSlider");
+const blueSlider = document.getElementById("blueSlider");
+const greenSlider = document.getElementById("greenSlider");
+const addColorButton = document.getElementById("colorButton");
+const colorBox = document.getElementById("colorBox");
+const drawingPad = document.getElementById("drawingPad");
+const ctx = drawingPad.getContext("2d");
+const clearButton = document.getElementById("clear");
+const emptyPalette = document.getElementById("emptyPalette");
 let red = redSlider.value;
 let blue = blueSlider.value;
 let green = greenSlider.value;
@@ -23,74 +25,84 @@ redSlider.addEventListener("mousedown", (e) => {
     redSlider.addEventListener("mousemove", (e) => {
         red = redSlider.value;
         colorViewer.style.backgroundColor = `rgb(${red},${green},${blue})`;
-    })
-})
+    });
+});
 
 greenSlider.addEventListener("mousedown", (e) => {
     greenSlider.addEventListener("mousemove", (e) => {
         green = greenSlider.value;
         colorViewer.style.backgroundColor = `rgb(${red},${green},${blue})`;
-    })
-})
+    });
+});
 
 blueSlider.addEventListener("mousedown", (e) => {
     blueSlider.addEventListener("mousemove", (e) => {
         blue = blueSlider.value;
         colorViewer.style.backgroundColor = `rgb(${red},${green},${blue})`;
-    })
-})
+    });
+});
 
 addColorButton.addEventListener("click", (e) => {
     addColor();
+});
+
+emptyPalette.addEventListener("click", (e) => {
+    let list = document.querySelectorAll('.addedColor');
+    for(i = 3; i < list.length; i++)
+    {
+        list[i].remove();
+    }
 })
 
-drawingPad.addEventListener('mousedown', (e) => {
+clearButton.addEventListener("click", (e) => {
+    ctx.clearRect(0, 0, 700, 375);
+});
+
+drawingPad.addEventListener("mousedown", (e) => {
     x = e.offsetX;
     y = e.offsetY;
     isDrawing = true;
-})
+});
 
-drawingPad.addEventListener('mousemove', (e) => {
+drawingPad.addEventListener("mousemove", (e) => {
     if (isDrawing == true) {
         drawLine(ctx, x, y, e.offsetX, e.offsetY);
         x = e.offsetX;
         y = e.offsetY;
     }
-})
+});
 
 drawingPad.addEventListener("mouseleave", (e) => {
     isDrawing = false;
-})
+});
 
-window.addEventListener('mouseup', (e) => {
+window.addEventListener("mouseup", (e) => {
     if (isDrawing === true) {
         drawLine(ctx, x, y, e.offsetX, e.offsetY);
         x = 0;
         y = 0;
         isDrawing = false;
     }
-})
+});
 
 //Functions
 
-function addColorEvents(){
-    let colors = document.getElementsByClassName('addedColor');
-    for (i = 0; i < colors.length; i++)
-    {
+function addColorEvents() {
+    let colors = document.getElementsByClassName("addedColor");
+    for (i = 0; i < colors.length; i++) {
         colors[i].addEventListener("click", (e) => {
-            document.querySelector(".selected").classList.remove('selected');
-            e.target.classList.add('selected');
+            document.querySelector(".selected").classList.remove("selected");
+            e.target.classList.add("selected");
             selectedColor = e.target.value;
             console.log(selectedColor);
-        })
+        });
     }
 }
 
-function addColor(){
-    if(numColors < 8)
-    {
-        let added = document.createElement('button');
-        added.classList.add('addedColor');
+function addColor() {
+    if (numColors < 8) {
+        let added = document.createElement("button");
+        added.classList.add("addedColor");
         added.value = `rgb(${red},${green},${blue})`;
         added.style.backgroundColor = `rgb(${red},${green},${blue})`;
         colorBox.append(added);
@@ -103,8 +115,8 @@ function drawLine(context, x1, y1, x2, y2) {
     context.beginPath();
     context.strokeStyle = selectedColor;
     context.lineWidth = 2;
-    context.moveTo(x1,y1);
-    context.lineTo(x2,y2);
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
     context.stroke();
     context.closePath();
 }
